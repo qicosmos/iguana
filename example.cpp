c192107af56462360f82d01639d77cbd9364bf37
Gogs <gogs@fake.local>
2017-02-07 17:38:08 +0800

    first version.

---
+++ example.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include "json.hpp"
+
+struct person
+{
+	std::string  name;
+	int          age;
+};
+REFLECTION(person, name, age)
+
+struct one_t
+{
+	int id;
+};
+REFLECTION(one_t, id);
+
+struct composit_t
+{
+	int a;
+	std::vector<std::string> b;
+	int c;
+	std::map<int, int> d;
+	std::unordered_map<int, int> e;
+	double f;
+	std::list<one_t> g;
+};
+REFLECTION(composit_t, a, b, c, d, e, f, g);
+
+//void performance()
+//{
+//	person obj;
+//	char * json = "{ \"Name\" : \"Boo\", \"Age\" : 28}";
+//	ajson::load_from_buff(obj, json);
+//
+//	const size_t LEN = 1000000;
+//
+//	//反序列化
+//	std::cout << "ajson deserialize: ";
+//	boost::timer t;
+//	for (size_t i = 0; i < LEN; i++)
+//	{
+//		ajson::load_from_buff(obj, json);
+//	}
+//	std::cout << t.elapsed() << std::endl;
+//
+//	std::cout << "reflib deserialize: ";
+//	t.restart();
+//	for (size_t i = 0; i < LEN; i++)
+//	{
+//		iguana::json::from_json(obj, json);
+//	}
+//	std::cout << t.elapsed() << std::endl;
+//
+//	//序列化
+//	std::cout << "ajson serialize: ";
+//	t.restart();
+//	ajson::string_stream ss1;
+//
+//	for (size_t i = 0; i < LEN; i++)
+//	{
+//		ss1.clear();
+//		ajson::save_to(ss1, obj);
+//	}
+//	std::cout << t.elapsed() << std::endl;
+//
+//	std::cout << "reflib serialize: ";
+//	t.restart();
+//
+//	iguana::json::string_stream ss;
+//	for (size_t i = 0; i < LEN; i++)
+//	{
+//		ss.clear();
+//		iguana::json::to_json(ss, obj);
+//	}
+//	std::cout << t.elapsed() << std::endl;
+//}
+
+int main()
+{
+	person p;
+	const char * json = "{ \"name\" : \"tom\", \"age\" : 28}";
+	iguana::json::from_json(p, json);
+
+	iguana::json::string_stream ss;
+	iguana::json::to_json(ss, p);
+	std::cout << ss.str() << std::endl;
+
+	one_t one = { 2 };
+	composit_t composit = { 1,{ "tom", "jack" }, 3,{ { 2,3 } },{ { 5,6 } }, 5.3,{ one } };
+	iguana::json::string_stream sst;
+	iguana::json::to_json(sst, composit);
+	std::cout << sst.str() << std::endl;
+
+	const char* str_comp = R"({"a":1, "b":["tom", "jack"], "c":3, "d":{"2":3,"5":6},"e":{"3":4},"f":5.3,"g":[{"id":1},{"id":2}])";
+	composit_t comp;
+	iguana::json::from_json(comp, str_comp);
+}
---
+++ traits.hpp
@@ -0,0 +1,60 @@
+//
+// Created by QY on 2017-01-05.
+//
+
+#ifndef SERIALIZE_TRAITS_HPP
+#define SERIALIZE_TRAITS_HPP
+
+#include <type_traits>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <deque>
+#include <queue>
+#include <list>
+namespace iguana
+{
+    template< class T >
+    struct is_signed_intergral_like : std::integral_constant < bool,
+            (std::is_integral<T>::value) &&
+            std::is_signed<T>::value
+    > {};
+
+    template< class T >
+    struct is_unsigned_intergral_like : std::integral_constant < bool,
+            (std::is_integral<T>::value) &&
+            std::is_unsigned<T>::value
+    > {};
+
+    template < template <typename...> class U, typename T >
+    struct is_template_instant_of : std::false_type {};
+
+    template < template <typename...> class U, typename... args >
+    struct is_template_instant_of< U, U<args...> > : std::true_type {};
+
+    template<typename T>
+    struct is_stdstring : is_template_instant_of < std::basic_string, T >
+    {};
+
+    template< class T >
+    struct is_sequence_container : std::integral_constant < bool,
+            is_template_instant_of<std::deque, T>::value ||
+            is_template_instant_of<std::list, T>::value ||
+            is_template_instant_of<std::vector, T>::value ||
+            is_template_instant_of<std::queue, T>::value
+    > {};
+
+    template< class T >
+    struct is_associat_container : std::integral_constant < bool,
+            is_template_instant_of<std::map, T>::value ||
+            is_template_instant_of<std::unordered_map, T>::value
+    > {};
+
+    template< class T >
+    struct is_emplace_back_able : std::integral_constant < bool,
+            is_template_instant_of<std::deque, T>::value ||
+            is_template_instant_of<std::list, T>::value ||
+            is_template_instant_of<std::vector, T>::value
+    > {};
+}
+#endif //SERIALIZE_TRAITS_HPP
