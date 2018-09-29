c393c8a623f636bcc189fabc1ce9d27614957dc8
Madokakaroto <indignentangel@gmail.com>
2018-09-29 22:40:13 +0800

    add new reflection

--- example/example.cpp
+++ example/example.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
-#include <iguana/json.hpp>
-#include <iguana/xml.hpp>
+#include <iguana/reflection_new.hpp>
 
 struct person
 {
 	std::string  name;
 	int          age;
 };
-REFLECTION(person, name, age)
+IGUANA_REFLECT(person, IGUANA_MDATA(name, age));
 
-struct one_t
+/*struct one_t
 {
 	int id;
 };
@@ -33,29 +32,10 @@ struct composit_t
 	double f;
 	std::list<one_t> g;
 };
-REFLECTION(composit_t, a, b, c, d, e, f, g);
+REFLECTION(composit_t, a, b, c, d, e, f, g);*/
 
 void test_json()
 {
-	person p;
-	const char * json = "{ \"name\" : \"tom\", \"age\" : 20}";
-	auto r = iguana::json::from_json0(p, json);
-
-	iguana::string_stream ss;
-	iguana::json::to_json(ss, p);
-	std::cout << ss.str() << std::endl;
-
-	one_t one = { 2 };
-	composit_t composit = { 1,{ "tom", "jack" }, 3,{ { 2,3 } },{ { 5,6 } }, 5.3,{ one } };
-	iguana::string_stream sst;
-	iguana::json::to_json(sst, composit);
-	std::cout << sst.str() << std::endl;
-
-//	const char* str_comp = R"({"a":1, "b":["tom", "jack"], "c":3, "d":{"2":3,"5":6},"e":{"3":4},"f":5.3,"g":[{"id":1},{"id":2}])";
-	const char* str_comp = R"({"b":["tom", "jack"], "a":1, "c":3, "e":{"3":4}, "d":{"2":3,"5":6},"f":5.3,"g":[{"id":1},{"id":2}])";
-	composit_t comp;
-	iguana::json::from_json0(comp, str_comp);
-	std::cout<<comp.a<<" "<<comp.f<<std::endl;
 }
 
 //void performance()
@@ -107,7 +87,7 @@ void test_json()
 //	std::cout << t.elapsed() << std::endl;
 //}
 
-void test_xml()
+/*void test_xml()
 {
 	person p = {"admin", 20};
 	iguana::string_stream ss;
@@ -123,10 +103,11 @@ void test_xml()
 	std::string xml = "			<?xml version=\"1.0\" encoding=\"UTF-8\">  <name>buke</name> <one><id>1</id></one>  <age>2</age>";
 	two t1;
 	iguana::xml::from_xml(t1, xml.data(), xml.length());
-}
+}*/
 
 int main()
 {
 	test_json();
-	test_xml();
+	//test_xml();
+    return 0;
 }
