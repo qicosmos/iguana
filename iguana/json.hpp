c553dd0af3263d34b645f2151537842b00db3de7
sunbo.kie <sunbo.kie@bytedance.com>
2020-11-10 17:08:54 +0800

    remove constexpr in some non-constexpr environment to fix compile bug in MacOS with clang

--- iguana/json.hpp
+++ iguana/json.hpp
@@ -1218,7 +1218,7 @@ namespace iguana { namespace json
 		}
 
 		template<typename T>
-		inline constexpr std::enable_if_t<is_tuple<std::decay_t<T>>::value, bool>
+		inline std::enable_if_t<is_tuple<std::decay_t<T>>::value, bool>
 			from_json(T&& t, const char *buf, size_t len = -1) {
 			using U = std::decay_t<T>;
 			g_has_error = false;
@@ -1232,7 +1232,7 @@ namespace iguana { namespace json
 		}
 
 		template<typename T>
-		inline constexpr std::enable_if_t<is_sequence_container<std::decay_t<T>>::value, bool> 
+		inline std::enable_if_t<is_sequence_container<std::decay_t<T>>::value, bool>
 			from_json(T&& v, const char *buf, size_t len = -1) {
 			v.clear();
 			using U = typename std::decay_t<T>::value_type;
@@ -1252,7 +1252,7 @@ namespace iguana { namespace json
 		}
 
         template<typename T>
-        inline constexpr std::enable_if_t<is_reflection_v<T>, bool> from_json(T &&t, const char *buf, size_t len = -1) {
+        inline std::enable_if_t<is_reflection_v<T>, bool> from_json(T &&t, const char *buf, size_t len = -1) {
             g_has_error = false;
             reader_t rd(buf, len);
             do_read(rd, t);
@@ -1261,7 +1261,7 @@ namespace iguana { namespace json
 
         //this interface support disorderly parse, however slower than from_json interface
         template<typename T, typename = std::enable_if_t<is_reflection<T>::value>>
-        inline constexpr bool from_json0(T &&t, const char *buf, size_t len = -1) {
+        inline bool from_json0(T &&t, const char *buf, size_t len = -1) {
             g_has_error = false;
             reader_t rd(buf, len);
             do_read0(rd, t);
