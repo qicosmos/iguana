c90e964d49759665568f809cf9f5bdffbb55fa4f
liuqiang <thorneliu@gmail.com>
2022-11-27 01:01:46 +0800

    test github_events.json

--- test/test_headers.h
+++ test/test_headers.h
@@ -1,8 +1,9 @@
 #pragma once
-#include "iguana/reflection.hpp"
 #include <map>
 #include <optional>
 #include <vector>
+
+#include "iguana/reflection.hpp"
 
 struct MyClass1 {
   double member0;
@@ -251,3 +252,222 @@ struct random_t {
   std::vector<random_element_t> result;
 };
 REFLECTION(random_t, id, jsonrpc, total, result);
+// github_events.json
+namespace githubEvents {
+struct user_t {
+  std::string gists_url;
+  std::string gravatar_id;
+  std::string url;
+  std::string type;
+  std::string avatar_url;
+  std::string subscriptions_url;
+  std::string organizations_url;
+  std::string received_events_url;
+  std::string repos_url;
+  std::string login;
+  std::string starred_url;
+  int id;
+  std::string events_url;
+  std::string followers_url;
+  std::string following_url;
+};
+REFLECTION(user_t, gists_url, gravatar_id, url, type, avatar_url,
+           subscriptions_url, organizations_url, received_events_url, repos_url,
+           login, starred_url, id, events_url, followers_url, following_url);
+
+struct page_t {
+  std::string page_name;
+  std::string html_url;
+  std::string title;
+  std::string sha;
+  std::optional<std::string> summary;
+  std::string action;
+};
+REFLECTION(page_t, page_name, html_url, title, sha, summary, action);
+
+struct pull_request_t {
+  std::optional<std::string> html_url;
+  std::optional<std::string> patch_url;
+  std::optional<std::string> diff_url;
+};
+REFLECTION(pull_request_t, html_url, patch_url, diff_url);
+
+struct forkee_t {
+  std::string full_name;
+  std::string stargazers_url;
+  std::string clone_url;
+  bool fork;
+  std::string url;
+  std::string tags_url;
+  std::string description;
+  std::string merges_url;
+  int forks;
+  std::string language;
+  bool __private;
+  std::string archive_url;
+  std::string collaborators_url;
+  std::string languages_url;
+  user_t owner;
+  std::string git_refs_url;
+  std::string labels_url;
+  std::string pushed_at;
+  std::string html_url;
+  std::string trees_url;
+  std::string forks_url;
+  std::string commits_url;
+  std::string branches_url;
+  std::string notifications_url;
+  std::string created_at;
+  bool has_issues;
+  std::string blobs_url;
+  std::string issues_url;
+  std::string compare_url;
+  int open_issues;
+  std::string contents_url;
+  std::string name;
+  std::string statuses_url;
+  std::string assignees_url;
+  int forks_count;
+  std::string updated_at;
+  std::string issue_events_url;
+  std::string ssh_url;
+  std::string subscribers_url;
+  std::optional<std::string> mirror_url;
+  bool __public;
+  bool has_wiki;
+  std::string git_commits_url;
+  std::string downloads_url;
+  int id;
+  std::string pulls_url;
+  bool has_downloads;
+  std::string issue_comment_url;
+  int watchers_count;
+  std::optional<std::string> homepage;
+  std::string hooks_url;
+  std::string subscription_url;
+  std::string milestones_url;
+  std::string events_url;
+  std::string svn_url;
+  std::string git_tags_url;
+  std::string teams_url;
+  std::string comments_url;
+  int open_issues_count;
+  std::string keys_url;
+  std::string contributors_url;
+  int size;
+  int watchers;
+  std::string git_url;
+};
+REFLECTION(forkee_t, full_name, stargazers_url, clone_url, fork, url, tags_url,
+           description, merges_url, forks, language, __private, archive_url,
+           collaborators_url, languages_url, owner, git_refs_url, labels_url,
+           pushed_at, html_url, trees_url, forks_url, commits_url, branches_url,
+           notifications_url, created_at, has_issues, blobs_url, issues_url,
+           open_issues, contents_url, name, statuses_url, assignees_url,
+           forks_count, updated_at, issue_events_url, ssh_url, subscribers_url,
+           mirror_url, __public, has_wiki, git_commits_url, downloads_url, id,
+           pulls_url, has_downloads, issue_comment_url, watchers_count,
+           homepage, hooks_url, subscription_url, milestones_url, events_url,
+           svn_url, git_tags_url, teams_url, comments_url, open_issues_count,
+           keys_url, contributors_url, size, watchers, git_url, compare_url);
+
+struct issue_t {
+  user_t user;
+  std::string url;
+  std::vector<std::string> labels;
+  std::string html_url;
+  std::string labels_url;
+  pull_request_t pull_request;
+  std::string created_at;
+  std::optional<std::string> closed_at;
+  std::optional<std::string> milestone;
+  std::string title;
+  std::string body;
+  std::string updated_at;
+  int number;
+  std::string state;
+  std::optional<user_t> assignee;
+  int id;
+  int comments;
+  std::string events_url;
+  std::string comments_url;
+};
+REFLECTION(issue_t, user, url, labels, html_url, labels_url, pull_request,
+           created_at, closed_at, milestone, title, body, updated_at, number,
+           state, assignee, id, comments, events_url, comments_url);
+
+struct comment_t {
+  user_t user;
+  std::string url;
+  std::string issue_url;
+  std::string created_at;
+  std::string body;
+  std::string updated_at;
+  int id;
+};
+REFLECTION(comment_t, user, url, issue_url, created_at, body, updated_at, id);
+
+struct actor_org_t {
+  std::string gravatar_id;
+  std::string login;
+  std::string avatar_url;
+  std::string url;
+  int id;
+};
+REFLECTION(actor_org_t, gravatar_id, login, avatar_url, url, id);
+
+struct repo_t {
+  std::string url;
+  int id;
+  std::string name;
+};
+REFLECTION(repo_t, url, id, name);
+
+struct author_t {
+  std::string email;
+  std::string name;
+};
+REFLECTION(author_t, email, name);
+
+struct commit_t {
+  std::string url;
+  std::string message;
+  bool distinct;
+  std::string sha;
+  author_t author;
+};
+REFLECTION(commit_t, url, message, distinct, sha, author);
+
+struct payload_t {
+  std::optional<std::vector<commit_t>> commits;
+  std::optional<int> distinct_size;
+  std::optional<int> push_id;
+  std::optional<std::string> head;
+  std::optional<std::string> before;
+  std::optional<int> size;
+  std::optional<forkee_t> forkee;
+  std::optional<std::vector<page_t>> pages;
+  std::optional<std::string> action;
+  std::optional<comment_t> comment;
+  std::optional<issue_t> issue;
+  std::optional<std::string> description;
+  std::optional<std::string> master_branch;
+  std::optional<std::string> ref;
+  std::optional<std::string> ref_type;
+};
+REFLECTION(payload_t, commits, distinct_size, push_id, head, before, size,
+           forkee, pages, action, comment, issue, description, master_branch,
+           ref, ref_type);
+
+struct event_t {
+  std::string type;
+  std::string created_at;
+  std::optional<actor_org_t> actor;
+  std::optional<actor_org_t> org;
+  repo_t repo;
+  bool __public;
+  payload_t payload;
+  std::string id;
+};
+REFLECTION(event_t, type, created_at, actor, org, repo, __public, payload, id);
+} // namespace githubEvents
--- test/test_json_files.cpp
+++ test/test_json_files.cpp
@@ -1,16 +1,18 @@
-#include "iguana/reflection.hpp"
 #include <filesystem>
 #include <stdexcept>
 #include <vector>
+
+#include "iguana/reflection.hpp"
 #define DOCTEST_CONFIG_IMPLEMENT
-#include "doctest.h"
-#include "iguana/json_reader.hpp"
-#include "test_headers.h"
 #include <filesystem>
+#include <iguana/json_reader.hpp>
 #include <iguana/json_util.hpp>
 #include <iguana/json_writer.hpp>
 #include <iostream>
 #include <limits>
+
+#include "doctest.h"
+#include "test_headers.h"
 
 TEST_CASE("test canada.json") {
   std::cout << std::filesystem::current_path().string() << "\n";
@@ -84,7 +86,7 @@ TEST_CASE("test numbers.json") {
   iguana::from_json_file(dummy, jsonName);
 
   CHECK(dummy.size() == numbers.size());
-  for (auto i = 0; i < numbers.size(); i++) {
+  for (size_t i = 0; i < numbers.size(); i++) {
     CHECK(numbers[i] - dummy[i] < std::numeric_limits<double>::epsilon());
   }
   if (std::filesystem::exists(jsonName))
@@ -116,6 +118,65 @@ TEST_CASE("test random.json") {
   iguana::from_json_file(t, "../data/random.json");
   CHECK(t.result.back().id == 1000);
   CHECK(t.result.back().age == 32);
+}
+
+TEST_CASE("test github_events.json") {
+  {
+    std::string one_event = R"([
+    {
+      "type": "PushEvent",
+      "created_at": "2013-01-10T07:58:30Z",
+      "actor": {
+        "gravatar_id": "a7cec1f75a06a5f8ab53139515da5d99",
+        "login": "jathanism",
+        "avatar_url": "https://secure.gravatar.com/avatar/a7cec1f75a06a5f8ab53139515da5d99?d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png",
+        "url": "https://api.github.com/users/jathanism",
+        "id": 138052
+      },
+      "repo": {
+        "url": "https://api.github.com/repos/jathanism/trigger",
+        "id": 6357414,
+        "name": "jathanism/trigger"
+      },
+      "public": true,
+      "payload": {
+        "commits": [
+          {
+            "url": "https://api.github.com/repos/jathanism/trigger/commits/05570a3080693f6e55244e012b3b1ec59516c01b",
+            "message": "- SSH Channel data now initialized in base class (TriggerSSHChannelBase)\n- New doc w/ checklist for adding new vendor support to Trigger.",
+            "distinct": true,
+            "sha": "05570a3080693f6e55244e012b3b1ec59516c01b",
+            "author": {
+              "email": "jathanism@aol.com",
+              "name": "jathanism"
+            }
+          }
+        ],
+        "distinct_size": 1,
+        "ref": "refs/heads/issue-22",
+        "push_id": 134107894,
+        "head": "05570a3080693f6e55244e012b3b1ec59516c01b",
+        "before": "7460e1588817b3f885fb4ec76ec2f08c7caf6385",
+        "size": 1
+      },
+      "id": "1652857722"
+    }])";
+    std::vector<githubEvents::event_t> events;
+    iguana::from_json(events, one_event);
+    CHECK(events.size() == 1);
+
+    auto &event = events.front();
+    CHECK(event.type == "PushEvent");
+    CHECK(event.created_at == "2013-01-10T07:58:30Z");
+    CHECK(event.actor);
+    CHECK(event.actor.value().gravatar_id ==
+          "a7cec1f75a06a5f8ab53139515da5d99");
+    CHECK(event.repo.url == "https://api.github.com/repos/jathanism/trigger");
+    CHECK(event.__public == true);
+    CHECK(event.id == "1652857722");
+  }
+  std::vector<githubEvents::event_t> events;
+  iguana::from_json_file(events, "../data/github_events.json");
 }
 
 // doctest comments
