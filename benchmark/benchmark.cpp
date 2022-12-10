2ec4d0e8e70ef4c264dfe59b804c7b1d472c7e7f
Courier Ma <courier_ma@outlook.com>
2022-12-10 11:18:53 +0800

    parse benchmark vs from_json

--- benchmark/benchmark.cpp
+++ benchmark/benchmark.cpp
@@ -1,5 +1,6 @@
 #include "iguana/json_reader.hpp"
 #include "iguana/json_writer.hpp"
+#include "iguana/value.hpp"
 #include <chrono>
 #include <iostream>
 #include <map>
@@ -24,7 +25,8 @@ public:
     if (m_ns)
       *m_ns = dur.count();
     else
-      std::cout << m_name << " : " << dur.count() << " ns\n";
+      std::cout << std::left << std::setw(45) << m_name << " : " << std::right
+                << std::setw(12) << dur.count() << " ns\n";
   }
 
 private:
@@ -201,7 +203,7 @@ void test_from_json(std::string filename, auto &obj, const auto &json_str,
                     const int size) {
   iguana::from_json(obj, std::begin(json_str), std::end(json_str));
 
-  std::string iguana_str = "iguana parse " + filename;
+  std::string iguana_str = "iguana from_json " + filename;
 
   {
     ScopedTimer timer(iguana_str.data());
@@ -222,6 +224,18 @@ void test_from_json(std::string filename, auto &obj, const auto &json_str,
     }
   }
 #endif
+}
+
+void test_dom_parse(std::string filename, const auto &json_str,
+                    const int size) {
+  std::string iguana_str = "iguana parse " + filename;
+  iguana::jvalue val;
+  {
+    ScopedTimer timer(iguana_str.data());
+    for (int i = 0; i < size; ++i) {
+      iguana::parse(val, json_str);
+    }
+  }
 }
 
 void test_to_json() {
@@ -274,12 +288,15 @@ void test_parse() {
       {"../data/instruments.json", instruments_t{}},
   };
 
+  iguana::jvalue val;
   for (auto &pair : test_map) {
     auto content = iguana::json_file_content(pair.first);
 
     std::visit(
         [&](auto &&arg) { test_from_json(pair.first, arg, content, 10); },
         pair.second);
+
+    test_dom_parse(pair.first, content, 10);
   }
 }
 
@@ -297,6 +314,12 @@ int main() {
   obj_t obj;
   for (int i = 0; i < 10; ++i) {
     test_from_json("obj_t", obj, json0, 100000);
+    std::cout << "====================\n";
+  }
+
+  iguana::jvalue val;
+  for (int i = 0; i < 10; ++i) {
+    test_dom_parse("obj_t", json0, 100000);
     std::cout << "====================\n";
   }
 }
