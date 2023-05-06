5239d7ca9bc2b085b6086400232614272fb44c05
bbbgan <2893129936@qq.com>
2023-05-07 02:42:07 +0800

    add xml benchmark

---
+++ benchmark/xml_benchmark.cpp
@@ -0,0 +1,76 @@
+#include "xml_bench.hpp"
+
+class ScopedTimer {
+public:
+  ScopedTimer(const char *name)
+      : m_name(name), m_beg(std::chrono::high_resolution_clock::now()) {}
+  ScopedTimer(const char *name, uint64_t &ns) : ScopedTimer(name) {
+    m_ns = &ns;
+  }
+  ~ScopedTimer() {
+    auto end = std::chrono::high_resolution_clock::now();
+    auto dur =
+        std::chrono::duration_cast<std::chrono::nanoseconds>(end - m_beg);
+    if (m_ns)
+      *m_ns = dur.count();
+    else
+      std::cout << std::left << std::setw(45) << m_name << " : " << std::right
+                << std::setw(12) << dur.count() << " ns\n";
+  }
+
+private:
+  const char *m_name;
+  std::chrono::time_point<std::chrono::high_resolution_clock> m_beg;
+  uint64_t *m_ns = nullptr;
+};
+
+std::string xml_file_content(const std::string &filename) {
+  std::error_code ec;
+  uint64_t size = std::filesystem::file_size(filename, ec);
+  if (ec) {
+    throw std::runtime_error("file size error " + ec.message());
+  }
+  if (size == 0) {
+    throw std::runtime_error("empty file");
+  }
+  std::string content;
+  content.resize(size);
+  std::ifstream file(filename, std::ios::binary);
+  file.read(content.data(), content.size());
+  return content;
+}
+
+void test_deserialize() {
+  int iterations = 1000;
+
+  std::cout << "============ deserialize rpm_filelists.xml  ===============\n";
+  std::vector<std::string> xmlfilelist(iterations);
+  for (int i = 0; i < iterations; ++i) {
+    xmlfilelist[i] = xml_file_content("../data/rpm_filelists.xml");
+  }
+  filelists_t filelists;
+  // filelists.package.reserve(100);
+  {
+    ScopedTimer timer("test deserialize rpm_filelists.xml");
+    for (int i = 0; i < iterations; ++i) {
+      iguana::xml::from_xml<rapidxml::parse_fastest>(filelists,
+                                                     xmlfilelist[i].data());
+    }
+  }
+
+  std::cout << "============ deserialize sample_rss.xml    ===============\n";
+  std::vector<std::string> xmlrss(iterations);
+  for (int i = 0; i < iterations; ++i) {
+    xmlrss[i] = xml_file_content("../data/sample_rss.xml");
+  }
+  rss_t rss;
+  // rss.channel.item.reserve(1000);
+  {
+    ScopedTimer timer("test deserialize sample_rss.xml");
+    for (int i = 0; i < iterations; ++i) {
+      iguana::xml::from_xml<rapidxml::parse_fastest>(rss, xmlrss[i].data());
+    }
+  }
+}
+
+int main() { test_deserialize(); }
