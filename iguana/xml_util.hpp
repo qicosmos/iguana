a0642bb6d3cef3cc039785bdd37c0562b9f23276
qicosmos <qicosmos@163.com>
2023-07-10 14:41:11 +0800

    format

--- iguana/xml_util.hpp
+++ iguana/xml_util.hpp
@@ -16,18 +16,18 @@
 namespace iguana {
 
 template <class T>
-concept char_t = std::same_as<std::decay_t<T>, char> ||
-                 std::same_as<std::decay_t<T>, char16_t> ||
-                 std::same_as<std::decay_t<T>, char32_t> ||
-                 std::same_as<std::decay_t<T>, wchar_t>;
+concept char_t = std::same_as < std::decay_t<T>,
+char > || std::same_as<std::decay_t<T>, char16_t> ||
+    std::same_as<std::decay_t<T>, char32_t> ||
+    std::same_as<std::decay_t<T>, wchar_t>;
 
 template <class T>
-concept bool_t = std::same_as<std::decay_t<T>, bool> ||
-                 std::same_as<std::decay_t<T>, std::vector<bool>::reference>;
+concept bool_t = std::same_as < std::decay_t<T>,
+bool > || std::same_as<std::decay_t<T>, std::vector<bool>::reference>;
 
 template <class T>
-concept int_t = std::integral<std::decay_t<T>> && !
-char_t<std::decay_t<T>> && !bool_t<T>;
+concept int_t =
+    std::integral<std::decay_t<T>> && !char_t<std::decay_t<T>> && !bool_t<T>;
 
 template <class T>
 concept float_t = std::floating_point<std::decay_t<T>>;
@@ -47,8 +47,8 @@ template <typename T>
 concept str_view_t = is_basic_string_view<std::remove_reference_t<T>>;
 
 template <class T>
-concept str_t = std::convertible_to<std::decay_t<T>, std::string_view> && !
-str_view_t<T>;
+concept str_t =
+    std::convertible_to<std::decay_t<T>, std::string_view> && !str_view_t<T>;
 
 template <class T>
 concept string_t = std::convertible_to<std::decay_t<T>, std::string_view>;
@@ -62,30 +62,30 @@ concept sequence_container_t =
 
 template <typename Type>
 concept unique_ptr_t = requires(Type ptr) {
-                         ptr.operator*();
-                         typename std::remove_cvref_t<Type>::element_type;
-                       } && !requires(Type ptr, Type ptr2) { ptr = ptr2; };
+  ptr.operator*();
+  typename std::remove_cvref_t<Type>::element_type;
+}
+&&!requires(Type ptr, Type ptr2) { ptr = ptr2; };
 
 template <typename Type>
 concept optional_t = requires(Type optional) {
-                       optional.value();
-                       optional.has_value();
-                       optional.operator*();
-                       typename std::remove_cvref_t<Type>::value_type;
-                     };
+  optional.value();
+  optional.has_value();
+  optional.operator*();
+  typename std::remove_cvref_t<Type>::value_type;
+};
 
 template <typename Type>
 concept container = requires(Type container) {
-                      typename std::remove_cvref_t<Type>::value_type;
-                      container.size();
-                      container.begin();
-                      container.end();
-                    };
+  typename std::remove_cvref_t<Type>::value_type;
+  container.size();
+  container.begin();
+  container.end();
+};
 template <typename Type>
-concept map_container =
-    container<Type> && requires(Type container) {
-                         typename std::remove_cvref_t<Type>::mapped_type;
-                       };
+concept map_container = container<Type> && requires(Type container) {
+  typename std::remove_cvref_t<Type>::mapped_type;
+};
 
 template <class T>
 concept plain_t =
@@ -93,25 +93,24 @@ concept plain_t =
 
 template <class T>
 concept c_array = std::is_array_v<std::remove_cvref_t<T>> &&
-                  std::extent_v<std::remove_cvref_t<T>> > 0;
+                  std::extent_v<std::remove_cvref_t<T>> >
+0;
 
 template <typename Type>
 concept array = requires(Type arr) {
-                  arr.size();
-                  std::tuple_size<std::remove_cvref_t<Type>>{};
-                };
+  arr.size();
+  std::tuple_size<std::remove_cvref_t<Type>>{};
+};
 
 template <typename Type>
-concept tuple = !
-array<Type> &&requires(Type tuple) {
-                std::get<0>(tuple);
-                sizeof(std::tuple_size<std::remove_cvref_t<Type>>);
-              };
+concept tuple = !array<Type> && requires(Type tuple) {
+  std::get<0>(tuple);
+  sizeof(std::tuple_size<std::remove_cvref_t<Type>>);
+};
 
 template <class T>
-concept non_refletable =
-    container<T> || c_array<T> || tuple<T> || optional_t<T> ||
-    unique_ptr_t<T> || std::is_fundamental_v<T>;
+concept non_refletable = container<T> || c_array<T> || tuple<T> ||
+    optional_t<T> || unique_ptr_t<T> || std::is_fundamental_v<T>;
 
 template <typename T, typename map_type = std::unordered_map<std::string_view,
                                                              std::string_view>>
