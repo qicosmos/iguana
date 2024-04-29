144ec79f351094e780c98f8f35b9897fb502941c
qicosmos <qicosmos@163.com>
2024-04-29 17:41:33 +0800

    add version

---
+++ iguana/version.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+// Note: Update the version when release a new version.
+
+// YLT_VERSION % 100 is the sub-minor version
+// YLT_VERSION / 100 % 1000 is the minor version
+// YLT_VERSION / 100000 is the major version
+#define IGUANA_VERSION 100004  // 1.0.4
