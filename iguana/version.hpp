7c350c082e51ea87f5a0522cdf28c668c83ea13e
qicosmos <qicosmos@163.com>
2024-04-29 17:42:19 +0800

    update

--- iguana/version.hpp
+++ iguana/version.hpp
@@ -2,7 +2,7 @@
 
 // Note: Update the version when release a new version.
 
-// YLT_VERSION % 100 is the sub-minor version
-// YLT_VERSION / 100 % 1000 is the minor version
-// YLT_VERSION / 100000 is the major version
+// IGUANA_VERSION % 100 is the sub-minor version
+// IGUANA_VERSION / 100 % 1000 is the minor version
+// IGUANA_VERSION / 100000 is the major version
 #define IGUANA_VERSION 100004  // 1.0.4
