29a91fec2a5828d9f838f8999356d3162f3ebf1e
qicosmos <qicosmos@163.com>
2024-07-09 14:29:53 +0800

    base

---
+++ iguana/ylt/reflection/field_names.hpp
@@ -0,0 +1,64 @@
+#pragma once
+#include <string_view>
+
+#include "object_to_tuple.hpp"
+
+namespace ylt::reflection {
+
+namespace internal {
+
+template <class T>
+struct Wrapper {
+  using Type = T;
+  T v;
+};
+
+template <class T>
+Wrapper(T) -> Wrapper<T>;
+
+// This workaround is necessary for clang.
+template <class T>
+inline constexpr auto wrap(const T& arg) noexcept {
+  return Wrapper{arg};
+}
+
+template <auto ptr>
+inline constexpr std::string_view get_field_name() {
+#if defined(_MSC_VER)
+  constexpr std::string_view func_name = __FUNCSIG__;
+#else
+  constexpr std::string_view func_name = __PRETTY_FUNCTION__;
+#endif
+
+#if defined(__clang__)
+  auto split = func_name.substr(0, func_name.size() - 2);
+  return split.substr(split.find_last_of(":.") + 1);
+#elif defined(__GNUC__)
+  auto split = func_name.substr(0, func_name.size() - 2);
+  return split.substr(split.find_last_of(":") + 1);
+#elif defined(_MSC_VER)
+  auto split = func_name.substr(0, func_name.size() - 7);
+  return split.substr(split.rfind("->") + 2);
+#else
+  static_assert(false,
+                "You are using an unsupported compiler. Please use GCC, Clang "
+                "or MSVC or switch to the rfl::Field-syntax.");
+#endif
+}
+}  // namespace internal
+
+template <typename T>
+inline constexpr std::array<std::string_view, members_count_v<T>>
+get_field_names() {
+  constexpr size_t Count = members_count_v<T>;
+  constexpr auto tp = internal::bind_fake_object_to_tuple<T>();
+
+  std::array<std::string_view, Count> arr;
+  [&]<size_t... Is>(std::index_sequence<Is...>) mutable {
+    ((arr[Is] = internal::get_field_name<internal::wrap(std::get<Is>(tp))>()),
+     ...);
+  }
+  (std::make_index_sequence<Count>{});
+  return arr;
+}
+}  // namespace ylt::reflection
---
+++ test/test_reflection.cpp
@@ -0,0 +1,45 @@
+#include "iguana/ylt/reflection/field_names.hpp"
+
+#define DOCTEST_CONFIG_IMPLEMENT
+#include "doctest.h"
+
+#if defined(__clang__) || defined(_MSC_VER) || \
+    (defined(__GNUC__) && __GNUC__ > 10)
+
+using namespace ylt::reflection;
+
+struct sub {
+  int id;
+};
+
+enum class Color { red, black };
+
+struct person {
+  Color color;
+  int id;
+  sub s;
+  std::string str;
+  int arr[2];
+};
+
+TEST_CASE("test field string") {
+  constexpr size_t size = members_count_v<person>;
+  CHECK(size == 5);
+  constexpr auto tp = internal::bind_fake_object_to_tuple<person>();
+  constexpr size_t tp_size = std::tuple_size_v<decltype(tp)>;
+  CHECK(tp_size == 5);
+
+  constexpr auto arr = get_field_names<person>();
+  for (auto name : arr) {
+    std::cout << name << ", ";
+  }
+  std::cout << "\n";
+  CHECK(arr ==
+        std::array<std::string_view, size>{"color", "id", "s", "str", "arr"});
+}
+
+#endif
+
+DOCTEST_MSVC_SUPPRESS_WARNING_WITH_PUSH(4007)
+int main(int argc, char **argv) { return doctest::Context(argc, argv).run(); }
+DOCTEST_MSVC_SUPPRESS_WARNING_POP
